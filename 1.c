68a1ee4ce2d9917c271eeeacecfab4d8cbc52259
Ayu <claire@ayu.land>
2025-03-15 21:50:01 +0800

    Use ncurses

--- 1.c
+++ 1.c
@@ -1,10 +1,11 @@
+#include <ncurses.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 const char N = 5;
 
-char A[N - 1][N], r0, s = 4, r, c, f;
+char A[N - 1][N], r0, s = 5, r, c, f;
 uint32_t rs;
 
 uint32_t my_rand()
@@ -18,7 +19,7 @@ void reset()
   r0 = my_rand() % N;
   r = -1; c = my_rand() % N; s = 1; f = 0;
 }
-void move()
+void M()
 {
   // 0 - unknown; 1 - known safe; 2 - monster
   if (r < 0 || A[r][c]) return;
@@ -37,25 +38,33 @@ void move()
 
 int main()
 {
+  initscr();
+  noecho();
+  cbreak();
   while (1) {
-    if (s == 4) {
+    if (s & 4) {
+      if (s == 4) {
+        printw("Press Enter to continue\n");
+        while (getch() != '\n') { }
+      }
       rs -= time(0) ^ clock() << 3;
       reset();
     }
-    printf("Move %d\n", s);
+    clear();
+    printw("Move %d\n", s);
     for (char i = -1; i < N; i++) {
       for (char j = 0; j < N; j++)
-        printf("%c%d%c", r == i && c == j ? '[' : ' ',
+        printw("%c%d%c", r == i && c == j ? '[' : ' ',
           i < 0 || i > N - 2 ? 3 : A[i][j], r == i && c == j ? ']' : ' ');
-      putchar('\n');
+      printw("\n");
     }
-    char m[10]; scanf("%s", m);
-    switch (m[0]) { case 'k': r -= 2; case 'j': r++; c++; case 'h': c -= 2; case 'l': c++; }
-    move();
-    if (r > N - 2) printf("\\(^ ^)/\n"), s++;
+    char m = getch();
+    switch (m) { case 'k': r -= 2; case 'j': r++; c++; case 'h': c -= 2; case 'l': c++; }
+    M();
+    if (r > N - 2) printw("\\(^ ^)/\n"), s++;
     else if (r >= 0 && r < N - 1 && A[r][c] == 2) {
       r = -1;
-      if (s++ == 3) printf("(> <)\n");
+      if (s++ == 3) printw("(> <)\n");
     }
   }
 }
