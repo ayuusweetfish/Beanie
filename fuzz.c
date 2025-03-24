72c85b383818bd55cf9fc0a3205b12d5893020e4
Ayu <claire@ayu.land>
2025-03-25 01:08:47 +0800

    Better organized fuzz target

--- fuzz.c
+++ fuzz.c
@@ -18,34 +18,36 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t n)
   s = 4;
   unsigned seed = *(uint32_t *)(data + 0);
   size_t p = 4;
+
+  // Run case
+  Y = seed;
+  printf("Restart, seed = %u\n", Y);
+  R(0);
   while (p < n) {
-    Y = seed;
-    printf("Restart, seed = %u\n", Y);
-    R(0);
-    while (p < n) {
-      uint8_t move = data[p++] % 4;
-      int8_t dir[4][2] = {{+1, 0}, {-1, 0}, {0, +1}, {0, -1}};
-      printf("Move %-5s | ", (const char *[]){"Down", "Up", "Right", "Left"}[move]);
-      r += dir[move][0];
-      c += dir[move][1];
-      M();
-      printf("(%2d,%2d) Step %d", r, c, s);
-      if (r > N - 2) {
-        assert(s == 3);
-        s = 4;
-        printf(" | Success!\n");
+    uint8_t move = data[p++] % 4;
+    int8_t dir[4][2] = {{+1, 0}, {-1, 0}, {0, +1}, {0, -1}};
+    printf("Move %-5s | ", (const char *[]){"Down", "Up", "Right", "Left"}[move]);
+    r += dir[move][0];
+    c += dir[move][1];
+    M();
+    printf("(%2d,%2d) Step %d", r, c, s);
+    if (r > N - 2) {
+      assert(s == 3);
+      s = 4;
+      printf(" | Success!\n");
+      break;
+    } else if (r >= 0 && A[r * N + c] == 2) {
+      r = -1;
+      printf(" | Bomb!");
+      if (s++ == 3) {
+        printf(" Fail!\n");
         break;
-      } else if (r >= 0 && A[r * N + c] == 2) {
-        r = -1;
-        printf(" | Bomb!");
-        if (s++ == 3) {
-          printf(" Fail!\n");
-          break;
-        }
       }
-      printf("\n");
     }
-    if (p < n) return -1;
+    printf("\n");
   }
+  // Do not add to corpus if trailing extraneous bytes
+  if (p < n) return -1;
+
   return 0;
 }
