5c60b28c6e955d6a2fd20c0ab01b5245cb8dae18
Ayu <claire@ayu.land>
2026-03-12 02:19:45 +0800

    Misleading ^ ^

--- 1.c
+++ 1.c
@@ -30,11 +30,11 @@ char M(unsigned R)
           R = (C = h[q]) % N, C /= N,
           I(R + T, C), I(~-R, C), I(R, C - T);
         F(r, Z) r[A] &= 3;
-        f |= 0 > (h - ~ - Z) * (n - !n);
+        f |= 0 > (h -~- Z) * (n - !n);
       }
     #define R { h = 0; F(c, N) h += b[c]; if (h == N
-      if (!r)
-        R) f |= 1;
+      if (!r ) R)
+        f |= 1;
         f |= r0++ && r0 < N & A[r0 - 2] & r0[b = A]; r0--;
       }
       f = 4 - (*a = -~(f & 1)) - s;
@@ -43,21 +43,21 @@ char M(unsigned R)
       *a = -~(s & 2 || c == r0 || r && 4 & Y && ~(r0 = c));
       f ^= r | c == r0;
     }
-    #define f ) { F(i, N) b[i] = A[(i - (i > N - 2)) * N + c] = t; b[c]++;
+  #define f ) { F(i, N) b[i] = A[(i - (i > N - 2)) * N + c] = t; b[c]++;
     if (b = a - c, t = 1, *a / 2 f }
     while (t--)
       for (b = A; b < Z + A; b += N)
         R-1) F(c, N) if (t = !c[b] f break; }
-        if ((C = b - A) && N == h + 2) {
-          F(r, N) Z |= !r[b - N] ^ !r[b];
-          F(r, Z) {
-            if (C - r && r != C - N && !(Z & 1))
-              F(c, N) !c[b] && (c[A + r] += (t = !A[r + c]));
-            r += Z / N;
-          }
-          Z &= -2;
+      if ((C = b - A) && N == h + 2) {
+        F(r, N) Z |= !r[b - N] ^ !r[b];
+        F(r, Z) {
+          if (C - r && r != C - N && !(Z & 1))
+            F(c, N) !c[b] && (c[A + r] += (t = !A[r + c]));
+          r += Z / N;
         }
+        Z &= -2;
       }
+    }
   }
 #undef f
 #undef R
@@ -97,7 +97,7 @@ int main(int t, char *argv[])
     for (signed char i = -1; i - N; i++)
       for (char j = 0; j < N; j++)
         putchar((putchar(!~i * putchar((*q = r - i || c - j) * -';' + 91) | 2 > N - i ? 61 : A[i * N + j][".*o"]), 0[q]["] "])),
-        j - ~ - N || putchar(10);
+        j -~- N || putchar(10);
     if (t > 3) {
       printf("\nPress Enter to continue\n");
       while (getchar() != '\n') { }
