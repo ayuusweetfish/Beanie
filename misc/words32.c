f555d66663807b6671828aee14518a2ed62b43c1
Ayu <claire@ayu.land>
2026-03-13 17:52:43 +0800

    Stranger long words

--- misc/words32.c
+++ misc/words32.c
@@ -12,13 +12,16 @@ int main()
   const char *S[] = {
     "\033[02A",
     "\033[K", "(O O)\r", "(> <)\n", "\\(>-<)/\n", "\\(^ ^)/\n", "~\\(^-^*)\n",
+    "Move 0\033[K",
+    "No audio ", "device su", "pplied, g", "ame will ", "be silent", " (_ _)\n",
+    "\nPress En", "ter to co", "ntinue\n",
   };
   for (int i = 0; i < sizeof S / sizeof S[0]; i++) {
     const char *s = S[i];
     uint64_t n = 0;
     for (int j = 0; j < 9 && s[j] != 0; j++)
       n |= ((uint64_t)s[j]) << (7 * j);
-    printf("%" PRIu64 "\n", n);
+    printf("%" PRIu64 " %" PRIx64 "\n", n, n);
     // w(n); printf("\n");
   }
 }
--- prog.c
+++ prog.c
@@ -81,7 +81,7 @@ void e(void)
   tcsetattr(a && fclose(a), TCSAFLUSH, &S);
 }
 
-void w(uint64_t n)
+void w(long long n)
 {
   while (n) putchar(n % 128), n >>= 7;
 }
@@ -92,21 +92,20 @@ int main(int T, char *argv[])
   U = S; U.c_lflag &= ~ECHO & ~ICANON;
   tcsetattr(atexit(e), 0, &U);
 
-  if (T && (a = fopen(argv[1], "wb"))) setbuf(a, 0);
-  else printf("No audio device supplied, game will be silent (_ _)\n");
+  T && (a = fopen(argv[1], "wb")) ? setbuf(a, 0) : (w(2368795716511872974), w(8495621981700207332), w(7440143561770350704), w(2367120232991520481), w(8421053345252782818), w(45414784422944));
 
   while (T = 2) {
     uint16_t S, E;
     if (4 & s) R(time(0) ^ clock() << 3); else T:T - 7 ? w(17553960347 + (N << 21) - 2619392 * (N & 8)) : exit(0);
-    for (signed char i = !(T++ & 4 || printf("Move %d\033[K\n", s)) - N; i - N * N;
+    for (signed char i = !(T++ & 4 || putchar((w(34359738368 * s + 0x4bb66d820cbdb7cd), 10))) - N; i - N * N;
       i += i + 2977 % putchar((putchar((i >> 7) * putchar((*B = T && r * N - i + c) * -';' + 91) | Z <= i ? 61 : A[i][".*o"]), 0[B]["] "])) && i % N -~- N || putchar(10));
     if (T > 4) {
-      printf("\nPress Enter to continue\n");
+      w(7965323610900178954), w(8054269566225527540), w(47482438056558);
       while (T = 10 - getchar())
         if (-T < s << 5 && s <~- (T = 2977 % T - ']' ? 1 | T % 8 : T / -13)) goto T;
       continue;
     }
-    for (int p = (E = ~s + fputs((const char *[]){"", "\033[K", "(O O)\r", "(> <)\n", "\\(>-<)/\n", "\\(^ ^)/\n", "~~\\(^-^*)\n"}[T = M(getchar())], stdout), (('3'-T*5)*T-154)*T+144), l = 3490 + ((T > 1) + (T > 2)) * 2400, i = (p && (p = 1309 / p)) & !E; a && fflush(a) || i - l; i += !a || 2 > fwrite(&S, 2, !!T, a))
+    for (int p = (w((T = E = M(getchar()))[(long long []){0, 145*145*';', 457848661928, 354729598760, 5837632671028316, 5837641771095132, 0xa52aaf2dbca2e7e}]), (('3'-T*5)*T-154)*T+144), l = 3490 + ((T > 1) + (T > 2)) * 2400, i = (p && (p = 1309 / p)) & !E; a && fflush(a) || i - l; i += !a || 2 > fwrite(&S, 2, !!T, a))
       S = (i < 1090 ? 54 > i % 109 : i < l - 2400 ? p ? i / (p / 2) & 1 : !((E *= 997) % 3) : 0) << 11;
     if (T ++> 2) goto T;
   }
