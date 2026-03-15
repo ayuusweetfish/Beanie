e5ae9da44a7b11a3bd1868e650a1c30ae280b1f6
Ayu <claire@ayu.land>
2026-03-15 17:34:24 +0800

    Test mark behaviour in fuzzer

--- fuzz.c
+++ fuzz.c
@@ -22,6 +22,8 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t n)
   Y = 0; R(seed);
   while (p < n) {
     uint8_t move = data[p++] % 4;
+    static char last_A[N * N - N];
+    for (int i = 0; i < N * N - N; i++) last_A[i] = A[i];
     char result = M('A' + move);
     for (signed int i = -1; i < N; i++) {
       for (int j = 0; j < N; j++)
@@ -34,13 +36,6 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t n)
     printf("Move %-5s | ", (const char *[]){"Up", "Down", "Right", "Left"}[move]);
     printf("(%2d,%2d) Step %d (%d)", r, c, o, result);
     assert(result >= 0 && result <= 5 /* && result != 4 */);
-    // Ensure that no row is empty!
-    for (int i = 0; i < N - 1; i++) {
-      char nonempty = 0;
-      for (int j = 0; j < N; j++)
-        if (A[i * N + j] % 2 != 1) { nonempty = 1; break; }
-      assert(nonempty);
-    }
     if (result == 4 || result == 5) {
       assert(r > N - 2);
       printf(" | Success!\n");
@@ -53,6 +48,19 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t n)
       }
     }
     printf("\n");
+
+    // Ensure that no row is empty!
+    for (int i = 0; i < N - 1; i++) {
+      char nonempty = 0;
+      for (int j = 0; j < N; j++)
+        if (A[i * N + j] % 2 != 1) { nonempty = 1; break; }
+      assert(nonempty);
+    }
+    // Ensure mark behaviour
+    for (int i = 0; i < N * N - N; i++) {
+      assert(A[i] >= 0 && A[i] <= 3);
+      if (A[i] == 3) assert(result <= 1 || last_A[i] == 0);
+    }
   }
   // Do not add to corpus if trailing extraneous bytes
   if (p < n) return -1;
